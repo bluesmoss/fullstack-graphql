6d95dabcdfdc2fa666a55d5b3fcb27d8de62622a
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2022-03-11 20:39:41 -0600

    chore(pets): add mutation to create items

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -18,21 +18,38 @@ query AllPets {
 
 `
 
+const NEW_PET = gql`
+mutation CreateAPet($newPet: NewPetInput!) {
+  addPet(input: $newPet) {
+    id
+    name
+    type
+    img
+  }
+}
+`
+
 export default function Pets () {
   const [modal, setModal] = useState(false)
   const {data, loading, error} = useQuery(ALL_PETS)
+  const  [createPet,  newPet] = useMutation(NEW_PET)
 
- 
-  if(loading){
-    return  <Loader />
-  }
-
-  if(error){
-    return  <p>Error!</p>
-  }
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: {
+        newPet: input
+      }
+    })
+  }
+
+  if(loading || newPet.loading){
+    return  <Loader />
+  }
+
+  if(error || newPet.error){
+    return  <p>Error!</p>
   }
   
   if (modal) {
