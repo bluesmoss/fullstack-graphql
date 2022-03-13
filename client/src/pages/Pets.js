c6d9a4fef3dafa56b05e97163d5fa3b016de72dd
Alfredo Morales Sánchez <bluesmoss@gmail.com>
2022-03-13 14:04:32 -0600

    chore(pets): add fragments

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -6,35 +6,35 @@ import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
 
+const PETS_FIELDS = gql`
+ fragment PetsFields on Pet{
+  id
+  name
+  type
+  img
+  owner {
+    id
+    age @client
+  }
+ }
+`
+
 const ALL_PETS = gql`
 query AllPets {
   pets {
-    id
-    name
-    type
-    img
-    owner {
-      id
-      age @client
-    }
+    ...PetsFields
   }
 }
-
+${PETS_FIELDS}
 `
 
 const NEW_PET = gql`
 mutation CreateAPet($newPet: NewPetInput!) {
   addPet(input: $newPet) {
-    id
-    name
-    type
-    img
-    owner {
-      id
-      age @client
-    }
+    ...PetsFields
   }
-}
+} 
+${PETS_FIELDS}
 `
 
 export default function Pets () {
